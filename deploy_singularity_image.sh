bc9b24273bb88d9bd87336a82d16d1317fefc807
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-02-08 20:59:55 +0000

    Add scripts and Dockerfile to deploy singularity image

---
+++ deploy_singularity_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Clean up old stuff
+rm -f deploy/docker/*.whl
+rm -f dist/*
+
+# Build and copy wheel
+python3 setup.py bdist_wheel
+cp dist/*.whl deploy/docker/arctool-0.2.0-py3-none-any.whl
+
+# Build docker image
+docker build -t jicscicomp/arctool deploy/docker/
+
+# Build singularity image
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ~/mnt/cluster_home/singularity/:/output \
+    --privileged \
+    -t \
+    --rm \
+    mcdocker2singularity \
+    jicscicomp/arctool \
+    arctool \
